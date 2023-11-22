b3878e64fb9b0f80cc7482010076a6eb345b7dee
Jacob Garby <garby@chalmers.se>
2023-11-22 17:03:33 +0100

    various bits

--- main.cpp
+++ main.cpp
@@ -10,21 +10,71 @@
 
 #include "graph.hpp"
 
-#define TILE_SPACING 30
-#define LINE_THICKNESS 7
-#define TILE_SIZE 5
+#define UPDATE_PERIOD 0.1
+#define TILE_SPACING 5
+#define LINE_THICKNESS 2
 #define HALF_SIZE ((float)TILE_SPACING / 2)
-#define TILE_OFFSET ((TILE_SPACING/2) - (TILE_SIZE/2))
 
-bool ccw(int ax, int ay, int bx, int by, int cx, int cy) {
-    return (cy-ay) * (bx-ax) > (by-ay) * (cx-ax);
+/* Returns:
+    0 if the A B C are colinear
+    1 if A B C are clockwise
+    2 if A B C are counterclockwise
+*/
+int orientation(int ax, int ay, int bx, int by, int cx, int cy) {
+    int o = (cy-ay) * (bx-ax) - (by-ay) * (cx-ax);
+    if (o == 0) return 0;
+    if (o > 0) return 1;
+    else return 2;
+}
+
+bool point_on_segment(int ax, int ay, int bx, int by, int px, int py) {
+    if ((px - ax) * (by - ay) == (bx - ax) * (py - ay)) {
+        if (std::min(ax, bx) <= px && px <= std::max(ax, bx) &&
+            std::min(ay, by) <= py && py <= std::max(ay, by)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool lines_cross(int ax, int ay, int bx, int by, int cx, int cy, int dx, int dy) {
-    return ccw(ax, ay, cx, cy, dx, dy) != ccw(bx, by, cx, cy, dx, dy) && ccw(ax, ay, bx, by, cx, cy) != ccw(ax, ay, bx, by, dx, dy);
+    // return ccw(ax, ay, cx, cy, dx, dy) != ccw(bx, by, cx, cy, dx, dy) && ccw(ax, ay, bx, by, cx, cy) != ccw(ax, ay, bx, by, dx, dy);
+    int o_abc = orientation(ax, ay, bx, by, cx, cy);
+    int o_abd = orientation(ax, ay, bx, by, dx, dy);
+
+    int o_cda = orientation(cx, cy, dx, dy, ax, ay);
+    int o_cdb = orientation(cx, cy, dx, dy, bx, by);
+    
+    if (o_abc != o_abd && o_cda != o_cdb) return true; // No colinearity, simple intersection
+
+    if (o_abc == 0 && point_on_segment(ax, ay, bx, by, cx, cy)) return true;
+    if (o_abd == 0 && point_on_segment(ax, ay, bx, by, dx, dy)) return true;
+    if (o_cda == 0 && point_on_segment(cx, cy, dx, dy, ax, ay)) return true;
+    if (o_cdb == 0 && point_on_segment(cx, cy, dx, dy, bx, by)) return true;
+
+    return false;
 }
 
-int dfs_step(Graph &G, std::stack<int> &stack) {
+bool node2node_intersects(int node_a_i, int node_b_i, Graph &G) {
+    Node &node_a = G.get_node(node_a_i);
+    Node &node_b = G.get_node(node_b_i);
+    
+    bool overlaps = false;
+    for (auto & [test_i, test_node] : G.get_nodes()) {
+        if (test_node.is_discovered() && test_node.get_prev() >= 0) {
+            if (test_i == node_a_i || test_node.get_prev() == node_a_i) continue; // Ignore simply extension of path
+            Node &test_prev = G.get_node(test_node.get_prev());
+            if (lines_cross(node_a.getx(), node_a.gety(), node_b.getx(), node_b.gety(), 
+                test_node.getx(), test_node.gety(), test_prev.getx(), test_prev.gety())) {
+                overlaps = true;
+                break;
+            }
+        }
+    }
+    return overlaps;
+}
+
+int dfs_step(Graph &G, std::stack<int> &stack, bool check_intersections=true, bool just_one_step=true) {
     while (!stack.empty()) {
         int next_i = stack.top();
         stack.pop(); 
@@ -33,27 +83,13 @@ int dfs_step(Graph &G, std::stack<int> &stack) {
         if (!next.is_discovered()) {
             next.set_discovered(true);
             for (int edge : next.get_neighbours()) {
-                bool overlaps = false;
-
-                
-                for (auto & [test_i, test_node] : G.get_nodes()) {
-                    if (test_node.is_discovered() && test_node.get_prev() >= 0) {
-                        Node &test_prev = G.get_node(test_node.get_prev());
-                        if (lines_cross(next.getx(), next.gety(), G.get_node(edge).getx(), G.get_node(edge).gety(), 
-                            test_node.getx(), test_node.gety(), test_prev.getx(), test_prev.gety())) {
-                            overlaps = true;
-                            break;
-                        }
-                    }
-                }
-
-                if (!overlaps) {
+                if (!check_intersections || !node2node_intersects(next_i, edge, G)) {
                     if (!G.get_node(edge).is_discovered())
                         G.get_node(edge).set_prev(next_i);
                     stack.push(edge);
                 }
             }
-            return next_i;
+            if (just_one_step) return next_i;
         }
     }
 
@@ -89,16 +125,24 @@ Graph make_grid_graph(int argc, char **argv, int *winwidth, int *winheight) {
             if (c > 0) node.add_neighbour(i - 1);
             if (c < cols - 1) node.add_neighbour(i + 1);
 
-            if (c > 1) node.add_neighbour(i - 2);
-            if (c < cols - 2) node.add_neighbour(i + 2);
+            // if (c > 1) node.add_neighbour(i - 2);
+            // if (c < cols - 2) node.add_neighbour(i + 2);
 
-            if (c > 2) node.add_neighbour(i - 3);
-            if (c < cols - 3) node.add_neighbour(i + 3);
+            // if (c > 19) node.add_neighbour(i - 20);
+            // if (c < cols - 20) node.add_neighbour(i + 20);
 
             // if (r > 0 && c > 0) node.add_neighbour(i - cols - 1);
             // if (r > 0 && c < cols - 1) node.add_neighbour(i - cols + 1);
             // if (r < rows - 1 && c > 0) node.add_neighbour(i + cols - 1);
             // if (r < rows - 1 && c < cols - 1) node.add_neighbour(i + cols + 1);
+
+            // if (r % 20 < 10) {
+            //     if (r > 0 && c > 2) node.add_neighbour(i - cols - 3);
+            //     if (r < rows - 1 && c < cols - 3) node.add_neighbour(i + cols + 3);
+            // } else {
+            //     if (r > 0 && c < cols - 3) node.add_neighbour(i - cols + 3);
+            //     if (r < rows - 1 && c > 2) node.add_neighbour(i + cols - 3);
+            // }
 
             // if (r > 1 && c > 1) node.add_neighbour(i - 2 * cols - 2);
 
@@ -164,11 +208,68 @@ Graph make_loose_graph(int argc, char **argv, int *winwidth, int *winheight) {
     return G;
 }
 
+Graph make_radial_graph(int argc, char **argv, int *winwidth, int *winheight) {
+    // argv = a.out radial [layers] [layer_height] [theta step] [angle between connecting nodes]
+
+    int layers, layer_height;
+    float theta_step, angle_between_connections;
+
+    if (argc == 6) {
+        layers = atoi(argv[2]);
+        layer_height = atoi(argv[3]);
+        theta_step = atof(argv[4]);
+        angle_between_connections = atof(argv[5]);
+    } else {
+        exit(EXIT_FAILURE);
+    }
+
+    Graph G;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    *winwidth = *winheight = layers * layer_height * 2 + 20; // 10px padding each side
+
+    int middle_node = G.add_node(Node(&G, *winwidth/2, *winheight/2, {})); // Center
+
+    for (int l = 0; l < layers; l++) {
+        float layer_radius = (float)layer_height * l;
+        int last_ni = -1;
+        int first_in_layer = -1;
+        
+        for (float theta = 0; theta < 2 * 3.14159; theta += theta_step) {
+            float xoff = std::sin(theta) * layer_radius;
+            float yoff = std::cos(theta) * layer_radius;
+            int new_ni = G.add_node(Node(&G, *winwidth/2 + (int)xoff, *winheight/2 + (int)yoff, {}));
+
+            if (first_in_layer == -1) first_in_layer = new_ni;
+
+            if (l == 1) G.get_node(middle_node).add_neighbour(new_ni);
+            if (last_ni >= 0) {
+                G.get_node(new_ni).add_neighbour(last_ni);
+                G.get_node(last_ni).add_neighbour(new_ni);
+            }
+            if (theta + theta_step >= 2 * 3.14159) {
+                G.get_node(first_in_layer).add_neighbour(new_ni);
+                G.get_node(new_ni).add_neighbour(first_in_layer);
+            }
+            if (l > 1) {
+                G.get_node(new_ni).add_neighbour(new_ni - (int)((2*3.14159)/theta_step) - 1);
+                G.get_node(new_ni - (int)((2 * 3.14159)/theta_step) - 1).add_neighbour(new_ni);
+            }
+            // if (l > 1 && l < layers-1) G.get_node(new_ni + (int)((2*3.14159)/theta_step)).add_neighbour(new_ni);
+
+            std::shuffle(G.get_node(new_ni).get_neighbours().begin(), G.get_node(new_ni).get_neighbours().end(), gen);
+
+            last_ni = new_ni;
+        }
+    }
+
+    return G;
+}
+
 int main(int argc, char **argv) {
     SetTraceLogLevel(LOG_ERROR);
 
-    std::cout << "Perpendicular touching lines cross? " << lines_cross(0,0, 5,0, 0,0, 0,5) << std::endl;
-    
     int winwidth, winheight;
 
     if (argc < 2) {
@@ -183,6 +284,8 @@ int main(int argc, char **argv) {
         G = make_grid_graph(argc, argv, &winwidth, &winheight);
     } else if (gtype == "loose") {
         G = make_loose_graph(argc, argv, &winwidth, &winheight);
+    } else if (gtype == "radial") {
+        G = make_radial_graph(argc, argv, &winwidth, &winheight);
     }
 
 
@@ -193,19 +296,28 @@ int main(int argc, char **argv) {
     double last_update_time = GetTime();
     int dfs_head = -1;
     int nodes_found = 0;
+    bool done = false;
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
 
         // for (auto & [node_i, node] : G.get_nodes()) {
-        //     DrawCircle(node.getx(), node.gety(), 2, GRAY);
+            // DrawCircle(node.getx(), node.gety(), 2, GRAY);
+            // DrawPixel(node.getx(), node.gety(), WHITE);
         // }
 
-        for (auto & [node_i, node] : G.get_nodes()) {
-            auto col = node.is_discovered() ? WHITE : GRAY;
-            if (node_i == dfs_head) col = RED;
+        if (IsKeyDown(KEY_N)) {
+            for (auto & [node_i, node] : G.get_nodes()) {
+                for (int to_i : node.get_neighbours()) {
+                    Node &to = G.get_node(to_i);
+                    DrawLine(node.getx(), node.gety(), to.getx(), to.gety(), ColorAlpha(WHITE, 0.15));
+                }
+                DrawCircle(node.getx(), node.gety(), 2, RED);
+            }
+        }
 
+        for (auto & [node_i, node] : G.get_nodes()) {
             if (node.is_discovered() && node.get_prev() >= 0) {
                 Node &maze_prev = G.get_node(node.get_prev());
                 if (maze_prev.is_discovered()) {
@@ -214,8 +326,8 @@ int main(int argc, char **argv) {
                         {(float)node.getx(), (float)node.gety()},
                         (float)LINE_THICKNESS, ColorAlpha(WHITE, 1)
                     );
-                    DrawCircle(maze_prev.getx(), maze_prev.gety(), (float)LINE_THICKNESS/2, WHITE);
-                    DrawCircle(node.getx(), node.gety(), (float)LINE_THICKNESS/2, WHITE);
+                    // DrawCircle(maze_prev.getx(), maze_prev.gety(), (float)LINE_THICKNESS/2, WHITE);
+                    // DrawCircle(node.getx(), node.gety(), (float)LINE_THICKNESS/2, WHITE);
                 }
             }
         }
@@ -239,12 +351,16 @@ int main(int argc, char **argv) {
         }
 
         double now = GetTime();
-        // if (true) {
-        if (now - last_update_time > 0.005) {
+        // if (!done) {
+        if (!done && now - last_update_time > UPDATE_PERIOD) {
         // if (IsKeyPressed(KEY_SPACE)) {
             last_update_time = now;            
-            dfs_head = dfs_step(G, dfs_stack);
+            dfs_head = dfs_step(G, dfs_stack, true, true);
             if (dfs_head != -1) nodes_found++;
+            else {
+                std::cout << "Finished calculating maze" << std::endl;
+                done = true;
+            }
         }
         
         EndDrawing();
--- readme.md
+++ readme.md
@@ -2,4 +2,8 @@
 
 You need raylib to run this (because of the realtime rendering of the maze). Either install it system-wide or within this folder, although for the latter you may need to adjust the `#include` in the source code.
 
+## TODO list
 
+ - Maintain a linked list of all discovered of discovered nodes, so that it's more efficient both to render the maze and to check for intersections.
+ - Implement some fun maze types, like a radial labrynth-type maze, and a maze that resembles an input image.
+ - A solver for the maze
